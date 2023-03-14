3f309d37aeee69671a68ee1bc861ac8681a5ab79
gert <gert@cross-dream.co.jp>
2023-03-14 17:48:31 +0900

    simple objects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { PerspectiveCamera, useTexture } from '@react-three/drei'
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -25,6 +27,44 @@ function Box(props) {
   )
 }
 
+function Floor(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+    
+  const colorMap = useLoader(TextureLoader, 'grass.jpg')
+  
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}>
+      <planeGeometry args={[15, 15, 1, 1]} />
+      <meshStandardMaterial map={colorMap} />
+    </mesh>
+  )
+}
+
+function Player(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  //useFrame((state, delta) => (ref.current.position.x += -0.1*delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      rotation = {[1.6,0,0]}>
+      <coneGeometry args={[1, 1.5, 4]} />
+      <meshStandardMaterial color={clicked ? 'grey' : 'blue'} />
+    </mesh>
+  )
+}
+
 export default function App() {
   return (
     <Canvas>
@@ -32,8 +72,9 @@ export default function App() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls />
+      <Player position={[1.2, 0, 0]} />
+      <Floor position={[0, 0, 0]} />
+      <PerspectiveCamera makeDefault position={[0, 0, 10]} />
     </Canvas>
   )
 }
