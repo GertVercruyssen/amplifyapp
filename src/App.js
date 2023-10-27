efbc65835fe6b1a075d2418e1fd9b67f0a4bbe12
gertcrossdream <128119847+gertcrossdream@users.noreply.github.com>
2023-10-27 17:33:14 +0900

    reset back to hello world

--- src/App.js
+++ src/App.js
@@ -1,80 +1,16 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { PerspectiveCamera, useTexture } from '@react-three/drei'
-import { TextureLoader } from "three/src/loaders/TextureLoader";
+import React from 'react';
+import logo from './logo.svg';
+import './app.css';
 
-
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+function App() {
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
 }
 
-function Floor(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-    
-  const colorMap = useLoader(TextureLoader, 'grass.jpg')
-  
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}>
-      <planeGeometry args={[15, 15, 1, 1]} />
-      <meshStandardMaterial map={colorMap} />
-    </mesh>
-  )
-}
-
-function Player(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  //useFrame((state, delta) => (ref.current.position.x += -0.1*delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      rotation = {[1.6,0,0]}>
-      <coneGeometry args={[1, 1.5, 4]} />
-      <meshStandardMaterial color={clicked ? 'grey' : 'blue'} />
-    </mesh>
-  )
-}
-
-export default function App() {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Player position={[1.2, 0, 0]} />
-      <Floor position={[0, 0, 0]} />
-      <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-    </Canvas>
-  )
-}
+export default App;
